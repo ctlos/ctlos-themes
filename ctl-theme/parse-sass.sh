2b3e137cc057efba0868280e4d46c5ecf7772185
creio <mailcreio@gmail.com>
2020-03-28 06:56:11 +0300

    2020.03.28 Update

---
+++ ctl-theme/parse-sass.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -ueo pipefail
+
+if [ ! "$(which sassc 2> /dev/null)" ]; then
+  echo sassc needs to be installed to generate the css.
+  exit 1
+fi
+
+_COLOR_VARIANTS=('' '-dark' '-light')
+if [ ! -z "${COLOR_VARIANTS:-}" ]; then
+  IFS=', ' read -r -a _COLOR_VARIANTS <<< "${COLOR_VARIANTS:-}"
+fi
+
+_SOLID_VARIANTS=('' '-solid')
+if [ ! -z "${SOLID_VARIANTS:-}" ]; then
+  IFS=', ' read -r -a _SOLID_VARIANTS <<< "${SOLID_VARIANTS:-}"
+fi
+
+SASSC_OPT="-M -t expanded"
+
+for color in "${_COLOR_VARIANTS[@]}"; do
+  for solid in "${_SOLID_VARIANTS[@]}"; do
+    sassc $SASSC_OPT src/gtk/gtk${color}${solid}.{scss,css}
+    echo ">> generating gtk${color}${solid}.css."
+  done
+done
+
+sassc $SASSC_OPT src/gnome-shell/gnome-shell.{scss,css}
+echo ">> generating gnome-shell.css."
+sassc $SASSC_OPT src/gnome-shell/gnome-shell-dark.{scss,css}
+echo ">> generating gnome-shell-dark.css."
---
+++ ctl-theme/src/xfwm4/render-asset-dark.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -ueo pipefail
+
+RENDER_SVG="$(command -v rendersvg)" || true
+INKSCAPE="$(command -v inkscape)" || true
+OPTIPNG="$(command -v optipng)" || true
+
+if [ -n "$INKSCAPE" ] ; then
+  if "$INKSCAPE" --help | grep -e "--export-png" > /dev/null; then
+    EXPORT_FILE_OPTION="--export-png"
+  else
+    EXPORT_FILE_OPTION="--export-file"
+  fi
+fi
+
+SRC_FILE="assets-dark.svg"
+ASSETS_DIR="assets-dark"
+
+i="$1"
+
+echo "Rendering '$ASSETS_DIR/$i.png'"
+if [[ -n "${RENDER_SVG}" ]]; then
+  "$RENDER_SVG" --export-id "$i" \
+                "$SRC_FILE" "$ASSETS_DIR/$i.png"
+else
+  "$INKSCAPE" --export-id="$i" \
+              --export-id-only \
+              "$EXPORT_FILE_OPTION"="$ASSETS_DIR/$i.png" "$SRC_FILE" >/dev/null
+fi
+if [[ -n "${OPTIPNG}" ]]; then
+  "$OPTIPNG" -o7 --quiet "$ASSETS_DIR/$i.png"
+fi
